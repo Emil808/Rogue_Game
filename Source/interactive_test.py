150ab8646d6b79b8653cf312d7d728561d3a390e
paolo <paolo.banzon@sjsu.edu>
2019-05-07 16:40:16 -0700

    made a menu

--- Source/interactive_test.py
+++ Source/interactive_test.py
@@ -5,21 +5,32 @@ from Source.Rogue import Hero
 from Source.Rogue import Monster
 import keyboard
 
-room0 = room()
-hero = Hero(room0)
-monster = Monster(room0)
+#print("Welcome to Rogue\n")
+#start = input("Press 'p' to Play\nPress 'q' to Quit")
 
-while hero.die(monster):  # edge case for hero and monster starting at same spot
-    hero = Hero(room0)
+ans = True
+while ans:
+    print("Welcome to Rogue\n")
+    start = input("Press 'p' to Play.\nPress 'q' to Quit.\n")
+    if start == 'p':
+        room0 = room()
+        hero = Hero(room0)
+        monster = Monster(room0)
 
-room0.print(hero, monster)
-print("Game Start\n")
+        while hero.die(monster):  # edge case for hero and monster starting at same spot
+            hero = Hero(room0)
 
-while 1:
-    hero.move()
-    monster.pursue_hero(hero.get_position(), room0.get_room_nodes())
-    room0.print(hero, monster)
+        room0.print(hero, monster)
+        print("Game Start:\n")
 
-    if hero.die(monster):
-        print("YOU LOSE!\nGAME OVER.\n")
-        break
+        while 1:
+            hero.move()
+            monster.pursue_hero(hero.get_position(), room0.get_room_nodes())
+            room0.print(hero, monster)
+
+            if hero.die(monster):
+                print("YOU LOSE!\nGAME OVER.\n")
+                break
+    elif start == 'q':
+        print("Thank you for playing.")
+        ans = False
